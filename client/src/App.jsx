4b01e846c35eee4262b326b5fc5aac0a52cebe42
Taquispro <121485751+Taquispro@users.noreply.github.com>
2024-03-27 11:26:42 +0530

    Routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
+import Projects from "./pages/Projects";
+
 function App() {
-  return <div className="text-3xl text-red-500">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
